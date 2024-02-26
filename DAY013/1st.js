a5fa2eedda85e3aae19602ba52163ceb2a85d2b6
Arisha598 <arishachaudhary56@gmail.com>
2024-02-26 06:24:57 +0530

    DAY012

---
+++ DAY013/1st.js
@@ -0,0 +1,8 @@
+//while loop
+let myarray =['asus' , 'lenovo','macbook']
+let arr=0
+while (arr < myarray.length) {
+    console.log(`the value is : ${myarray[arr]}`);
+    arr = arr+1
+    
+}
---
+++ DAY013/2nd.js
@@ -0,0 +1,7 @@
+//do-while loop
+
+let score = 12
+do {
+    console.log(score);
+    score++
+} while (score < 10);
