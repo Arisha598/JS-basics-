0a82d169202dd40fc01d2bf75a7c94be355852a3
Arisha598 <arishachaudhary56@gmail.com>
2024-03-06 22:36:15 +0530

    DAY017

---
+++ DAY017/index.js
@@ -0,0 +1,10 @@
+
+var a =document.querySelector(".container")
+var b =document.querySelector("i")
+a.addEventListener('dblclick',function(){
+   b.style.transform='translate(-50%,-50%)scale(1)'
+
+   setTimeout(function(){
+    b.style.transform='translate(-50%,-50%)scale(0)'
+   },2000)
+})
